c2c328a84976903cf2b4aa959e8f65370fe006b2
Qingyun Tao <qingyunha@gmail.com>
2015-01-07 12:03:30 +0800

    add suspend and continue play function to play.rb

--- play.rb
+++ play.rb
@@ -3,22 +3,38 @@ require 'rainbow/ext/string'
 bn = File.basename ARGV[0], '.mp3'
 su = bn + '.lrc'
 
-player = Thread.new {
+mpgpid = fork {
   `mpg123 #{ARGV[0]}` 
 }
 
-ftime = 0
-File.open(su).each_line do |line|
-  time = line[1..8]
-  min, sec = time.split ':' 
-  time = min.to_i * 60 + sec.to_f
-  sleep time - ftime
-  print line[10..-1].color(:green)
-  ftime = time
-end
+subpid = fork { 
+  ftime = 0
+  File.open(su).each_line do |line|
+    time = line[1..8]
+    min, sec = time.split ':' 
+    time = min.to_i * 60 + sec.to_f
+    sleep time - ftime
+    print line[10..-1].color(:green)
+    ftime = time
+  end
+} 
 
-play.join
-
+sleep 1  #make sure mpg123 has already run
+mpgpid = `ps u | grep mpg123`.split[1]
+sig = 0
+Signal.trap('TSTP'){
+  if sig == 0
+    Process.kill(:TSTP, subpid.to_i)
+    Process.kill(:TSTP, mpgpid.to_i)
+    sig = 1
+  else
+    Process.kill(:CONT, mpgpid.to_i)
+    sleep 1.5 #try to synchronize mpg and subtitle
+    Process.kill(:CONT, subpid.to_i)
+    sig = 0
+  end
+}
+sleep
 
 if __FILE__ == $0
   gem 'minitest'
